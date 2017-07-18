b5d35b67f2d1becbfe9c8f79849a0db3f56e9553
Megan Pearson <megan@savant.no>
2017-07-18 11:17:53 +0200

    Implements burger with full screen overlay menu for small screens

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,9 +8,20 @@
 
 $(document).foundation();
 console.log("hi from app.js");
+console.log(Foundation.MediaQuery.current);
+console.log(Foundation.version);
 
+// Custom JS
+$(function() {
+  hljs.initHighlightingOnLoad();
+});
 
+$("#menu-button").click(function(){
+  $(".overlay-menu").toggle();
+  $("#menu-button").toggleClass("is-active");
+});
 
-$(document).ready(function() {
-   hljs.initHighlightingOnLoad();
+$("#menu-button-close").click(function(){
+  $(".overlay-menu").toggle();
+  $("#menu-button").toggleClass("is-active");
 });
